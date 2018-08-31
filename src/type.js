b4f760d4ce478ce5e5cc066db1f12ee55f0f5e00
hamger <2313303800@qq.com>
2018-08-31 11:27:35 +0800

    判断是否是移动端

--- src/type.js
+++ src/type.js
@@ -61,3 +61,16 @@ export function isEmptyArr (arg) {
   }
   return false
 }
+
+// 判断是否是移动端
+export function isMobile () {
+  if (
+    navigator.userAgent.match(
+      /(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i
+    )
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
